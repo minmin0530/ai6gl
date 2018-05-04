634f064f7b7c6c991e6ae58445223c86026481df
izumi_yoshiki <izumiyoshiki@icloud.com>
2018-05-04 21:24:24 +0900

    ベベルのエッジの途中

--- src/bevelcube.js
+++ src/bevelcube.js
@@ -3,6 +3,35 @@ class BevelCube {
     this.alpha1 = 1.0;
     this.alpha2 = 0.5;
     this.color = [
+    //top
+      0.5, 0.5, 0.5, this.alpha1,
+      0.5, 0.5, 0.5, this.alpha1,
+      0.5, 0.5, 0.5, this.alpha1,
+      0.5, 0.5, 0.5, this.alpha1,
+      0.5, 0.5, 0.5, this.alpha1,
+      0.5, 0.5, 0.5, this.alpha1,
+
+      0.5, 0.5, 0.5, this.alpha1,
+      0.5, 0.5, 0.5, this.alpha1,
+      0.5, 0.5, 0.5, this.alpha1,
+      0.5, 0.5, 0.5, this.alpha1,
+      0.5, 0.5, 0.5, this.alpha1,
+      0.5, 0.5, 0.5, this.alpha1,
+
+      0.5, 0.5, 0.5, this.alpha1,
+      0.5, 0.5, 0.5, this.alpha1,
+      0.5, 0.5, 0.5, this.alpha1,
+      0.5, 0.5, 0.5, this.alpha1,
+      0.5, 0.5, 0.5, this.alpha1,
+      0.5, 0.5, 0.5, this.alpha1,
+
+      0.5, 0.5, 0.5, this.alpha1,
+      0.5, 0.5, 0.5, this.alpha1,
+      0.5, 0.5, 0.5, this.alpha1,
+      0.5, 0.5, 0.5, this.alpha1,
+      0.5, 0.5, 0.5, this.alpha1,
+      0.5, 0.5, 0.5, this.alpha1,
+    //side  
       0.5, 0.5, 0.5, this.alpha1,
       0.5, 0.5, 0.5, this.alpha1,
       0.5, 0.5, 0.5, this.alpha1,
@@ -31,7 +60,7 @@ class BevelCube {
       1.0, 0.0, 0.0, this.alpha1,
       1.0, 0.0, 0.0, this.alpha1,
 
-
+//plane
       1.0, 0.0, 0.0, this.alpha2,
       1.0, 0.0, 0.0, this.alpha2,
       1.0, 0.0, 0.0, this.alpha2,
@@ -77,7 +106,7 @@ class BevelCube {
     const SIZE = 3.0;
     const BEVEL = 0.5;
     this.position = [
-      //edge
+      //edge top
       -SIZE,  SIZE,        -SIZE -BEVEL,
        SIZE,  SIZE +BEVEL, -SIZE,
        SIZE,  SIZE,        -SIZE -BEVEL,
@@ -105,8 +134,35 @@ class BevelCube {
       -SIZE -BEVEL,  SIZE,        -SIZE,
       -SIZE,         SIZE +BEVEL, -SIZE,
       -SIZE,         SIZE +BEVEL,  SIZE,
+      //edge side
+      SIZE,        -SIZE, -SIZE -BEVEL,
+      SIZE +BEVEL,  SIZE, -SIZE,
+      SIZE,         SIZE, -SIZE -BEVEL,
+      SIZE,        -SIZE, -SIZE -BEVEL,
+      SIZE +BEVEL, -SIZE, -SIZE,
+      SIZE +BEVEL,  SIZE, -SIZE,
 
-      //plane
+      SIZE +BEVEL, -SIZE,  SIZE,       
+      SIZE,         SIZE,  SIZE +BEVEL,
+      SIZE +BEVEL,  SIZE,  SIZE,       
+      SIZE +BEVEL, -SIZE,  SIZE,       
+      SIZE,        -SIZE,  SIZE +BEVEL,
+      SIZE,         SIZE,  SIZE +BEVEL,
+
+      -SIZE,        -SIZE,  -SIZE -BEVEL,
+      -SIZE -BEVEL,  SIZE,  -SIZE,
+      -SIZE,         SIZE,  -SIZE -BEVEL,
+      -SIZE,        -SIZE,  -SIZE -BEVEL,
+      -SIZE -BEVEL, -SIZE,  -SIZE,
+      -SIZE -BEVEL,  SIZE,  -SIZE,
+
+      -SIZE -BEVEL, -SIZE,  SIZE,       
+      -SIZE,         SIZE,  SIZE +BEVEL,
+      -SIZE -BEVEL,  SIZE,  SIZE,       
+      -SIZE -BEVEL, -SIZE,  SIZE,       
+      -SIZE,        -SIZE,  SIZE +BEVEL,
+      -SIZE,         SIZE,  SIZE +BEVEL,
+     //plane
       -SIZE,  SIZE, -SIZE -BEVEL,
        SIZE,  SIZE, -SIZE -BEVEL,
        SIZE, -SIZE, -SIZE -BEVEL,
@@ -150,7 +206,7 @@ class BevelCube {
        SIZE, -SIZE -BEVEL, -SIZE, 
     ];
     this.normal = [];
-    for (var v = 0; v < 20*9; v += 9) {
+    for (var v = 0; v < 28*9; v += 9) {
       var vec1 = [ this.position[3+v]-this.position[0+v], this.position[4+v]-this.position[1+v], this.position[5+v]-this.position[2+v] ];
       var vec2 = [ this.position[6+v]-this.position[0+v], this.position[7+v]-this.position[1+v], this.position[8+v]-this.position[2+v] ];
       this.normal.push(vec1[1] * vec2[2] - vec1[2] * vec2[1]);
